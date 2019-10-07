cb35318618bb8e04177772c45b1d69c738535bb4
yetinight <gang@ucsd.edu>
2019-10-07 11:26:41 -0500

    added a second unit test to test helper func

--- Palindrome/Palindrome.Test/UnitTest1.cs
+++ Palindrome/Palindrome.Test/UnitTest1.cs
@@ -11,12 +11,28 @@ namespace Palindrome.Test
         {
             //arrange
             PalindromeChecker pal = new PalindromeChecker();
-            string str = "abccba";
+            string str1 = "abccba";
+            string str2 = "abb";
 
             //act
-            bool isPal = pal.isPalindrome(str);
-            
+            bool isPal1 = pal.isPalindrome(str1);
+            bool isPal2 = pal.isPalindrome(str2);
+
             //assert
+            Assert.True(isPal1);
+            Assert.False(isPal2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            PalindromeChecker pal = new PalindromeChecker();
+            string str1 = "a,b, s";
+
+            string str2 = pal.removePuncAndSpaces(str1);
+
+            Assert.Equal("abs", str2);
+
         }
     }
 }
