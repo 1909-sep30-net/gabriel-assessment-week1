b2e3b9633d33de4a0091afc3fd238f1717308131
yetinight <gang@ucsd.edu>
2019-10-07 11:06:41 -0500

    created a helper method for palindrome checker

--- Palindrome/Palindrome.Library/Palindrome.cs
+++ Palindrome/Palindrome.Library/Palindrome.cs
@@ -4,7 +4,40 @@ using System.Text;
 
 namespace Palindrome.Library
 {
-    class Palindrome
+    public class Palindrome
     {
+        /*
+         * Returns whether or not the given string input is a palindrome
+         * 
+         */
+        public bool isPalindrome(string str)
+        {
+            // check if null
+            if (str == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // Removes punctuation from a string
+        public string removePuncAndSpaces(string str) 
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                // if current char in str is punctuation or a space, skip inserting current char
+                if (Char.IsPunctuation(str[i]) || str[i] == ' ')
+                {
+                    i += 1;
+                } else
+                {
+                    result += str[i];
+                }
+            }
+            return result;
+        }
+
     }
 }
